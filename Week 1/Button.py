43603ce0fc8903c273bd9d70e415d8542d476d52
Chill Guy - Chill Life <duongtuan2005.cs@gmail.com>
2026-09-15 13:15:31 +0700

    temp

--- Week 1/Button.py
+++
@@ -1,9 +0,0 @@
-from gpiozero import Button    # Khai bao su dung ham trong thu vien
-
-button = Button(2)    # Khai bao chan GPIO 2 la chan nhan nut
-
-while True:
-    if button.is_pressed:    # Kiem tra nut co duoc nhan hay khong
-        print("Button is pressed")    # In ra thong bao khi nut duoc nhan
-    else:
-        print("Button is not pressed")    # In ra thong bao khi nut khong duoc nhan
--- Week 1/HC-SR04.py
+++
@@ -1,8 +0,0 @@
-from gpiozero import DistanceSensor    # Khai bao su dung ham trong thu vien
-from time import sleep    # Khai bao thu vien time de dung ham sleep
-
-sensor = DistanceSensor(echo=23, trigger=24)
-
-while True:
-    print('Distance: ', sensor.distance, 'm')    # In ra khoang cach do duoc
-    sleep(1)    # Cho den 1 giay
